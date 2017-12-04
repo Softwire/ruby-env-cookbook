f78ffd8aa2aea8a0251f100d063e6c8a0e3ab585
Daniel Waters <daniel.waters@softwire.com>
2017-12-04 12:19:08 +0000

    woops now with ruby config not just mysql config

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,6 +5,6 @@ default[:mysql][:password] = 'password'
 default[:mysql][:action] = [:create, :start]
 default[:mysql][:include_client] = true
 
-default[:mysql][:rbenv][:version] = '2.4.2'
-default[:mysql][:rbenv][:user] = 'vagrant'
-default[:mysql][:rbenv][:gems] = [{name: 'bundler', version: '1.16.0'}]
+default[:rbenv][:version] = '2.4.2'
+default[:rbenv][:user] = 'vagrant'
+default[:rbenv][:gems] = [{name: 'bundler', version: '1.16.0'}]
